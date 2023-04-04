41c783d5b4d3f8661f08eec62b93856f51019c80
kanwar-sikandar-ali <kanwarsikandarali@gmail.com>
2023-04-04 07:03:59 +0500

    add code

---
+++ MERN-STACK-NOTEBOOK-BACKEND/routes/todoRoutes.js
@@ -0,0 +1,21 @@
+const { Router } = require("express");
+
+const { getToDo,saveToDo,updateToDo,deleteToDo } = require("../controllers/todoController");
+
+const router = Router();
+
+router.get("/", getToDo);
+
+router.post("/save", saveToDo);
+ 
+router.post("/update", updateToDo);
+
+router.post("/delete", deleteToDo);
+
+// router.get("/",(req,res)=>{
+
+//     res.json({message:"Hi there ..."})
+
+// })
+
+module.exports = router;
